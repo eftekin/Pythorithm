b7b39b5e448098b1fa071e04b527b2a39733ea5a
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-05-09 15:47:33 +0300

    feat(048_list_sum.py): create a Python program to find the sum of elements in a list

---
+++ basic_programs_daily/048_list_sum.py
@@ -0,0 +1,10 @@
+# Write a Python program to find sum of elements in list
+
+numbers = [10, 20, 30, 40, 60]
+sum_of_numbers = 0
+
+
+for i in numbers:
+    sum_of_numbers += i
+
+print("Sum of elements in the list:", sum_of_numbers)
