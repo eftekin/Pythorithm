1a8ba32b1ca3a3db76bce8f0547411ecc5ed2962
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-05-09 15:44:56 +0300

    feat(pronic_numbers.py): add a Python program to print all pronic numbers between 1 and 100. Pronic numbers are products of two consecutive integers, n and (n + 1), representing a rectangle.

---
+++ basic_programs_daily/047_pronic_numbers.py
@@ -0,0 +1,17 @@
+# Write a Python program to print all pronic numbers between 1 and 100.
+
+# A pronic number, also known as an oblong number or rectangular number, is a type of
+# figurate number that represents a rectangle. It is the product of two consecutive integers, n
+# and (n + 1).
+
+
+def is_pronic_number(num):
+    for n in range(1, int(num**0.5) + 1):
+        if n * (n + 1) == num:
+            return True
+
+
+print("Pronic numbers between 1 and 100 are: ")
+for i in range(1, 101):
+    if is_pronic_number(i):
+        print(i, end=" | ")
