36e96e501b673e98a53e5248fd438f0d8ab30b44
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-05-09 16:08:54 +0300

    feat(list_multiply.py): add a Python program to multiply all numbers in a list for daily practice.

---
+++ basic_programs_daily/049_list_multiply.py
@@ -0,0 +1,9 @@
+# Write a Python program to Multiply all numbers in the list.
+
+numbers = [10, 20, 30, 40, 50]
+product_of_numbers = 1
+
+for i in numbers:
+    product_of_numbers *= i
+
+print("Product of elements in the list:", product_of_numbers)
