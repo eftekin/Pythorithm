2f52069303f54ab69e88ca4445b84fe8f67617d0
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-05-15 21:24:48 +0300

    feat(056_list_empty_list.py): add Python program to remove empty lists from a list of lists.

---
+++ basic_programs_daily/056_list_empty_list.py
@@ -0,0 +1,6 @@
+# Write a Python program to Remove empty List from List.
+
+list_of_lists = [[1, 2, 3], [], [4, 5], [], [6, 7, 8], [], [9, 10]]
+filtered_list = [i for i in list_of_lists if i]
+
+print("List after removing empty lists:", filtered_list)
