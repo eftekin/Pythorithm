cf4ab585c4885b0edfa9aebeabb099d96ac5dc8f
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-06-04 14:29:24 +0300

    fix(build_bst.py): change return value from string "No Child" to None for consistency and clarity.

--- algorithms/01-recursion/build_bst.py
+++ algorithms/01-recursion/build_bst.py
@@ -1,6 +1,6 @@
 def build_bst(my_list):
     if len(my_list) == 0:
-        return "No Child"
+        return None
     middle_idx = len(my_list) // 2
     middle_value = my_list[middle_idx]
     print(f"Middle index: {middle_idx}")
