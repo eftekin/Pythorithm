b7ca02f1144a15a5300327eaffa2acdd9c917508
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-06-04 14:23:33 +0300

    feat(multiplication.py): add recursive function to calculate multiplication of two numbers with tests

---
+++ algorithms/01-recursion/multiplication.py
@@ -0,0 +1,10 @@
+def multiplication(num_a, num_b):
+    if num_a == 0 or num_b == 0:
+        return 0
+
+    return num_a + multiplication(num_a, num_b - 1)
+
+
+print(multiplication(3, 7) == 21)
+print(multiplication(5, 5) == 25)
+print(multiplication(0, 4) == 0)
