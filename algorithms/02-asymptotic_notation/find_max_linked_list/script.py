a8cc79d294dea7ad95ba6f2d64750d17096d6910
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-06-05 21:23:03 +0300

    feat(script.py): add script to find the maximum value in a linked list using linkedlist helper methods

---
+++ algorithms/02-asymptotic_notation/find_max_linked_list/script.py
@@ -0,0 +1,41 @@
+from linkedlist import LinkedList
+
+
+def find_max(linked_list):
+    print("--------------------------")
+    print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))
+    current = linked_list.get_head_node()
+    maximum = current.get_value()
+    while current.get_next_node():
+        current = current.get_next_node()
+        val = current.get_value()
+        if val > maximum:
+            maximum = val
+    return maximum
+
+
+ll = LinkedList(6)
+ll.insert_beginning(32)
+ll.insert_beginning(-12)
+ll.insert_beginning(48)
+ll.insert_beginning(2)
+ll.insert_beginning(1)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll)))
+
+ll_2 = LinkedList(60)
+ll_2.insert_beginning(12)
+ll_2.insert_beginning(22)
+ll_2.insert_beginning(-10)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_2)))
+
+ll_3 = LinkedList("A")
+ll_3.insert_beginning("X")
+ll_3.insert_beginning("V")
+ll_3.insert_beginning("L")
+ll_3.insert_beginning("D")
+ll_3.insert_beginning("Q")
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_3)))
+
+
+runtime = "N"
+print("The runtime of find_max is O({0})".format(runtime))
