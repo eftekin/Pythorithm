64bfeba0b2d08ab77091b6b46d3ff25eb9b63343
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-06-07 02:39:57 +0300

    feat(pattern_search.py): add a naive pattern searching algorithm to find a pattern within a given text for various test cases

---
+++ algorithms/03-pattern_searching/naive_pattern_search.py
@@ -0,0 +1,26 @@
+def pattern_search(text, pattern):
+    print("Input Text:", text, "Input Pattern:", pattern)
+    for index in range(len(text)):
+        print("Text Index:", index)
+        match_count = 0
+        for char in range(len(pattern)):
+            print("Pattern Index:", char)
+            if pattern[char] == text[index + char]:
+                match_count += 1
+            else:
+                break
+        if match_count == len(pattern):
+            print(pattern, "found at index", index)
+
+
+# TEST
+text = "HAYHAYNEEDLEHAYHAYHAYNEEDLEHAYHAYHAYHAYNEEDLE"
+pattern = "NEEDLE"
+text2 = "SOMEMORERANDOMWORDSTOpatternSEARCHTHROUGH"
+pattern2 = "pattern"
+text3 = "This   still      works with    spaces"
+pattern3 = "works"
+text4 = "722615457824612704202682179992552072047396"
+pattern4 = "42"
+
+pattern_search(text2, pattern2)
