1ba32559f76660756613547b61da385b0c37338b
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-06-07 01:52:24 +0300

    refactor(linkedlist.py): move linkedlist.py and node.py to a more logical location inside the linked_list directory feat(script.py): add sort_linked_list() function to sort the linked list in ascending order feat(script.py): print the sorted linked list after finding the maximum value in each linked list docs(script.py): document the runtime complexity of find_max and sort_linked_list functions

--- algorithms/02-asymptotic_notation/linked_list/script.py
+++ algorithms/02-asymptotic_notation/linked_list/script.py
@@ -14,6 +14,19 @@ def find_max(linked_list):
     return maximum
 
 
+def sort_linked_list(linked_list):
+    print("\n---------------------------")
+    print("The original linked list is:\n{0}".format(linked_list.stringify_list()))
+    new_linked_list = LinkedList()
+
+    while linked_list.get_head_node():
+        current_max = find_max(linked_list)
+        linked_list.remove_node(current_max)
+        new_linked_list.insert_beginning(current_max)
+
+    return new_linked_list
+
+
 ll = LinkedList(6)
 ll.insert_beginning(32)
 ll.insert_beginning(-12)
@@ -21,12 +34,16 @@ ll.insert_beginning(48)
 ll.insert_beginning(2)
 ll.insert_beginning(1)
 print("The maximum value in this linked list is {0}\n".format(find_max(ll)))
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll).stringify_list()))
+
 
 ll_2 = LinkedList(60)
 ll_2.insert_beginning(12)
 ll_2.insert_beginning(22)
 ll_2.insert_beginning(-10)
 print("The maximum value in this linked list is {0}\n".format(find_max(ll_2)))
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll_2).stringify_list()))
+
 
 ll_3 = LinkedList("A")
 ll_3.insert_beginning("X")
@@ -35,7 +52,10 @@ ll_3.insert_beginning("L")
 ll_3.insert_beginning("D")
 ll_3.insert_beginning("Q")
 print("The maximum value in this linked list is {0}\n".format(find_max(ll_3)))
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll_3).stringify_list()))
 
+runtime_find_max = "N"
+print("The runtime of find_max is O({0})".format(runtime_find_max))
 
-runtime = "N"
-print("The runtime of find_max is O({0})".format(runtime))
+runtime_sort = "N^2"
+print("The runtime of sort_linked_list is O({0})\n\n".format(runtime_sort))
