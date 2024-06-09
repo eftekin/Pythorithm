68e210202d0cd9233a6578dc497aecf1b5b43c88
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-06-09 20:39:58 +0300

    fix(sorts.py): updated bubble_sort and quicksort functions to accept a comparison function parameter for flexibility in sorting based on different criteria feat(utils.py): added code to convert author and title to lowercase in load_books function for standardization and ease of comparison.

--- algorithms/04-sorting_algorithms/a_sorted_tale/sorts.py
+++ algorithms/04-sorting_algorithms/a_sorted_tale/sorts.py
@@ -1,13 +1,13 @@
 import random
 
 
-def bubble_sort(arr):
+def bubble_sort(arr, comparison_function):
     swaps = 0
     sorted = False
     while not sorted:
         sorted = True
         for idx in range(len(arr) - 1):
-            if arr[idx] > arr[idx + 1]:
+            if comparison_function(arr[idx], arr[idx + 1]):
                 sorted = False
                 arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
                 swaps += 1
@@ -15,7 +15,7 @@ def bubble_sort(arr):
     return arr
 
 
-def quicksort(list, start, end):
+def quicksort(list, start, end, comparison_function):
     if start >= end:
         return
     pivot_idx = random.randrange(start, end + 1)
@@ -23,9 +23,9 @@ def quicksort(list, start, end):
     list[end], list[pivot_idx] = list[pivot_idx], list[end]
     less_than_pointer = start
     for i in range(start, end):
-        if pivot_element > list[i]:
+        if comparison_function(pivot_element, list[i]):
             list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
             less_than_pointer += 1
     list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
-    quicksort(list, start, less_than_pointer - 1)
-    quicksort(list, less_than_pointer + 1, end)
+    quicksort(list, start, less_than_pointer - 1, comparison_function)
+    quicksort(list, less_than_pointer + 1, end, comparison_function)
--- algorithms/04-sorting_algorithms/a_sorted_tale/utils.py
+++ algorithms/04-sorting_algorithms/a_sorted_tale/utils.py
@@ -8,7 +8,7 @@ def load_books(filename):
     with open(filename) as file:
         shelf = csv.DictReader(file)
         for book in shelf:
-            # add your code here
-
+            book["author_lower"] = book["author"].lower()
+            book["title_lower"] = book["title"].lower()
             bookshelf.append(book)
     return bookshelf
