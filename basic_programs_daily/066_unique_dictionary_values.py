cccf7d029a5762f7513dec8836e002a2b2386599
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-06-24 22:09:14 +0300

    feat(066_unique_dictionary_values.py): add a new python script to find unique values in a dictionary and print them

---
+++ basic_programs_daily/066_unique_dictionary_values.py
@@ -0,0 +1,10 @@
+my_dict = {"a": 10, "b": 20, "c": 10, "d": 30, "e": 20}
+
+uni_val = set()
+
+for i in my_dict.values():
+    uni_val.add(i)
+
+unique_values_list = list(uni_val)
+
+print("Unique values in the dictionary:", unique_values_list)
