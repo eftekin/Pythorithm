7ffa06adb11df90e79c85eeefb586d69225b2db7
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-10-18 22:59:03 +0300

    feat(096_correct_signs.py): add function to check if inequality expression is correct

---
+++ basic_programs_daily/096_correct_signs.py
@@ -0,0 +1,19 @@
+# Create a function that returns True if a given inequality expression is correct and
+# False otherwise.
+
+# Examples
+# correct_signs("3 < 7 < 11") ➞ True
+# correct_signs("13 > 44 > 33 <1") ➞ False
+# correct_signs("1 < 2 < 6 < 9 > 3") ➞ True
+
+
+def correct_signs(expression):
+    try:
+        return eval(expression)
+    except ():
+        return False
+
+
+print(correct_signs("3 < 7 < 11"))
+print(correct_signs("13 > 44 > 33 < 1"))
+print(correct_signs("1 < 2 < 6 < 9 > 3"))
