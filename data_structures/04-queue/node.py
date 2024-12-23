5e19216b62954307f7e685b68f5119148bc8306e
eftekin <m_eftekin@hotmail.com>
2024-12-23 23:23:00 +0300

    feat: add docstrings to Node and Queue classes for improved documentation

--- data_structures/04-queue/node.py
+++ data_structures/04-queue/node.py
@@ -1,13 +1,19 @@
 class Node:
+    """A Node class for linked data structures."""
+
     def __init__(self, value, next_node=None):
+        """Initialize a Node with a value and optional next_node."""
         self.value = value
         self.next_node = next_node
 
     def set_next_node(self, next_node):
+        """Set the next node reference."""
         self.next_node = next_node
 
     def get_next_node(self):
+        """Get the next node reference."""
         return self.next_node
 
     def get_value(self):
+        """Get the node's value."""
         return self.value
--- data_structures/04-queue/queue.py
+++ data_structures/04-queue/queue.py
@@ -2,13 +2,17 @@ from node import Node
 
 
 class Queue:
+    """A Queue implementation using a linked list structure."""
+
     def __init__(self, max_size=None):
+        """Initialize an empty queue with optional max_size."""
         self.head = None
         self.tail = None
         self.max_size = max_size
         self.size = 0
 
     def enqueue(self, value):
+        """Add an item to the end of the queue."""
         if self.has_space():
             item_to_add = Node(value)
             print("Adding " + str(item_to_add.get_value()) + " to the queue!")
@@ -22,7 +26,8 @@ class Queue:
         else:
             print("Sorry, no more room!")
 
-    def dequeue(self, value):
+    def dequeue(self):
+        """Remove and return the first item in the queue."""
         if self.get_size() > 0:
             item_to_remove = self.head
             print("Removing " + str(item_to_remove.get_value()) + " from the queue!")
@@ -37,23 +42,23 @@ class Queue:
             print("This queue is totally empty!")
 
     def peek(self):
+        """Return the first item in the queue without removing it."""
         if self.is_empty():
             print("Nothing to see here!")
         else:
             return self.head.get_value()
 
     def get_size(self):
+        """Return the number of items in the queue."""
         return self.size
 
     def has_space(self):
+        """Check if there is space in the queue."""
         if self.max_size is None:
             return True
         else:
             return self.max_size > self.get_size()
 
     def is_empty(self):
+        """Check if the queue is empty."""
         return self.size == 0
-
-
-q = Queue()
-q.enqueue("all the fluffy kitties")
